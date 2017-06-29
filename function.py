2bb41dc0084e5ab8e9a104530e58ec9fc7903557
Yanjun-Li <liyanjunthu@163.com>
2017-06-29 13:03:59 +0800

    更新0629
    更新了一些函数

--- function.py
+++ function.py
@@ -2,6 +2,7 @@
 #
 
 import pandas as pd
+import numpy as np
 import pickle
 
 def ADD(A,B):
@@ -21,42 +22,44 @@ def DEVIDE(A,B):
 def RANK(A):
     return A.rank(axis='columns', method='min', ascending=False)
 
-#在从今天开始往前看t日，第几天是出现最大值的地方，今天是第0天，昨天是第一天
+#在从今天开始往前看t日，第几天是出现最大值的地方，今天是第t-1天，昨天是第t-2天
 def TimeSeries_ArgMax(A,t):
-    pass
+    return A.rolling(t).apply(np.argmax)
 
 def POWER(A,t):
     pass
 
 def STD(A,t):
-    pass
+    return A.rolling(t).std()
 
 def CORR(A,B,t):
     pass
 
 def DELTA(A,t):
-    pass
+    return A - A.rolling(t).apply(np.choose(0))
 
 def SUM(A,t):
-    pass
+    return A.rolling(t).sum()
 
 def ABS(A,t):
     pass
 
-def MEAN(A):
-    pass
+def MEAN(A,t):
+    return A.rolling(t).mean()
 
 def IF(A,B,C):
     pass
 
 def TimeSeries_MIN(A,t):
-    pass
+    return A.rolling(t).min()
 
 def TimeSeries_MAX(A,t):
-    pass
+    return A.rolling(t).max()
 
+#----------------------
 def SIGN(A):
     pass
+    return A.sign()
 
 def COV(A,B,t):
     pass
@@ -77,6 +80,9 @@ if __name__ == "__main__":
     with open('closePrice.pkl', 'rb') as file2:
         B = pickle.load(file2)
 
-    C = RANK(A)
+    C = DELTA(A,10)
+    #C = TimeSeries_MIN(A,10)
+    D = MEAN(A,2)
 
     print C
+    #print D
